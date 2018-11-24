4b9239cdaacbf6d85907e5b123667d041e1ca8c9
anchor_cable <forever.blue.moon@gmail.com>
2018-11-24 11:17:58 +0900

    whitenoise上手く動かしたい

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,5 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
