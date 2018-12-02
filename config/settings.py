1e9c348dee5e6811d9d50f11cc725e63049f0b36
anchor_cable <forever.blue.moon@gmail.com>
2018-12-02 20:46:59 +0900

    template

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,7 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = False
+TEMPLATE_DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
